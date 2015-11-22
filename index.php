ada01716c50b03a52016303280ddf3ca61f00e8c
Andrej Karadzic <karadza3a@yahoo.com>
2015-11-22 23:05:42 +0100

    Updated client ID due to dev console bug

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<script type="text/javascript">
 			// Your Client ID can be retrieved from your project in the Google
 			// Developer Console, https://console.developers.google.com
-			var CLIENT_ID = '964072569290-gdpoo81np0q3rddbaujdh8jdd6vravu4.apps.googleusercontent.com';
+			var CLIENT_ID = '997372761535-10k4lovop0tc9iuo1099khm7354undpk.apps.googleusercontent.com';
 
 			var SCOPES = ["https://www.googleapis.com/auth/calendar"];
 
