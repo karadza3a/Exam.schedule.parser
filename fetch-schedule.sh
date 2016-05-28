31b204fc78278caea33fa4ddb15f1fe5148be6fd
Andrej Karadzic <karadza3a@yahoo.com>
2016-05-28 16:07:20 +0200

    Update fetch-schedule.sh

--- fetch-schedule.sh
+++ fetch-schedule.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BASE_URL="./files/sched"
-PDF_URL="http://www.raf.edu.rs/Rasporedi/Kolokvijumske%20nedelje/2015-2016/III_kol_nedelja.pdf"
+PDF_URL="http://www.raf.edu.rs/Rasporedi/Kolokvijumske%20nedelje/2015-2016/IV_kol_nedelja.pdf"
 
 curl -s $PDF_URL > $BASE_URL".pdf" ;
 /usr/local/bin/pdftotext -enc UTF-8 -layout $BASE_URL".pdf" ;
