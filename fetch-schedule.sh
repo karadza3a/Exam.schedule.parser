80366cf04b725d8e323d9032abffede2ddd8a00c
Andrej Karadzic <andrej.karadzic@gmail.com>
2017-04-19 11:25:47 +0200

    Updated to April 2017 schedule

--- fetch-schedule.sh
+++ fetch-schedule.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BASE_URL="./files/sched"
-PDF_URL="http://raf.edu.rs/Rasporedi/Ispitni%20rokovi/2016-2017/februarski.pdf"
+PDF_URL="https://www.raf.edu.rs/Rasporedi/Kolokvijumske%20nedelje/2016-2017/kol3.pdf"
 
 curl -s $PDF_URL > $BASE_URL".pdf" ;
 /usr/local/bin/pdftotext -enc UTF-8 -layout $BASE_URL".pdf" ;
